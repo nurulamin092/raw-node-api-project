332e77d1bfc0ff1c17fa6da0f4fcba9c6b205743
nurulamin092 <mnurulamin092@gmial.com>
2022-09-25 13:01:57 +0600

    token deleted api

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -144,7 +144,40 @@ handler._token.put = (requestProperties, callback) => {
 };
 
 handler._token.delete=(requestProperties,callback)=>{
-    
+    const id = 
+    typeof requestProperties.queryStringObject.id ==='string' &&
+    requestProperties.queryStringObject.id.trim().length === 20 ?
+    requestProperties.queryStringObject.id: false;
+
+    if (id) {
+        data.read('tokens',id,(err1,tokenData)=>{
+            if (!err1 && tokenData) {
+                data.delete('tokens',id,(err2)=>{
+                    if (!err2) {
+                        callback(200,{
+                            message:'Token  was successfully deleted!'
+                        });
+                    } 
+                    else{
+                        callback(500,{
+                            error:'There was a server side error!.'
+                        });
+                    }
+                });
+            }
+            else{
+                callback(500,{
+                    error:'There was a server side error!.'
+                })
+            }
+        });  
+    }
+    else{
+        callback(400,{
+            error:'There was a problem in your request....3'
+        })
+    }
+
    
 
 }
