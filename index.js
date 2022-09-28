1f604385bdc58e13cf833dc1b2d23b4cd6e048db
nurulamin092 <mnurulamin092@gmial.com>
2022-09-28 07:20:42 +0600

    worker file loop is worded

--- index.js
+++ index.js
@@ -7,63 +7,25 @@
  */
 
 //dependencies
-const http = require('http');
-const environment = require('./helpers/environments');
-const {handleReqRes}  = require('./helpers/handleReqRes');
-const { sendTwilioSms } = require('./helpers/notifications');
-const data = require('./lib/data');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
+
 //App object - module scaffolding
 const app ={};
 
 
-sendTwilioSms ('01916252884','Hello world',(err)=>{
-    console.log(`This is the error`,err);
-});
-
-// configuration
-
-app.config = {
-    port: 3000
-}
-//testing file
-//pore muse di
-
-// data.create('test','newFile',{name:'Bangladesh',language:'Bengali'},(err)=>{
-//     console.log(`error was`,err);
-// });
-
-// data read
-// data.read('test','newFile',(err,data)=>{
-//     console.log(data,err);
-// })
-
-// data update
-
-// data.update('test','newFile',{name:"Lakshmipur",language:"Noakhali"},(err)=>{
-//     console.log(err);
-// })
-
-// file delete
-// data.delete('test','newFile',(err)=>{
-//     console.log(err);
-// })
-//configuration
-// app.config ={
-   
-// };
-
-// create server
-app.createServer = ()=>{
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port,()=>{
-        console.log(`server listening to port at ${environment.port}`);
-    });
+app.init = () => {
+     // start the server 
+    server.init();
+    // start the workers
+    workers.init();
 };
 
-//handle Request Response
-app.handleReqRes = handleReqRes; 
+app.init();
 
-// server start 
-app.createServer(); 
+// export the app
+
+module.exports = app;   
+
 
 
